664342a96114960e502b02d29f6146cfcd3e0a8a
redccoma <redccoma@gmail.com>
2023-04-19 18:48:42 +0900

    OpenAI 기능 추가.
    - https://platform.openai.com/docs/api-reference 기능을 사용하기 쉽게 코드로 구현중.
    - 한글 처리를 위한 Font 파일 추가.
    - 프로젝트 셋팅에서 Android 패키지명, arm64, il2cpp 설정.

---
+++ Assets/Scripts/Core/OpenAI.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// https://platform.openai.com/docs/api-reference 기능들
+/// </summary>
+public static class OpenAI
+{
+    /// <summary>
+    /// 현재 사용 가능한 모델을 나열하고 소유자 및 가용성과 같은 각 모델에 대한 기본 정보를 제공합니다.
+    /// </summary>
+    private static readonly string GET_LIST_MODEL = "https://api.openai.com/v1/models";
+    /// <summary>
+    /// 소유자 및 권한과 같은 모델에 대한 기본 정보를 제공하는 모델 인스턴스를 검색합니다.
+    /// </summary>
+    private static readonly string GET_RETRIEVE_MODEL = "https://api.openai.com/v1/models/{0}";
+    /// <summary>
+    /// 제공된 프롬프트 및 매개변수에 대한 완성을 생성합니다.
+    /// </summary>
+    private static readonly string POST_CREATE_COMPLETION = "https://api.openai.com/v1/completions";
+}
